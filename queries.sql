254a1885126d71af0f25b5f0eb68b71c170ced10
Metaverse-Nabeel <nabeelahmed3129@gmail.com>
2023-06-06 02:21:13 +0500

    Update queries

--- queries.sql
+++ queries.sql
@@ -6,14 +6,14 @@ SELECT * FROM animals WHERE name LIKE '%mon';
 
 -- List the name of all animals born between 2016 and 2019.
 
-SELECT *
+SELECT name
 FROM animals
 WHERE
     date_of_birth BETWEEN '2016-01-01' AND '2019-01-01';
 
 -- List the name of all animals that are neutered and have less than 3 escape attempts.
 
-SELECT * FROM animals WHERE neutered AND (escape_attempts < 3);
+SELECT name FROM animals WHERE neutered AND (escape_attempts < 3);
 
 -- List the date of birth of all animals named either "Agumon"  or "Pikachu".
 
