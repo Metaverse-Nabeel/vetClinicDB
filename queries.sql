60e69ad4e96bf134d9b219750fc6a625fe066226
Metaverse-Nabeel <nabeelahmed3129@gmail.com>
2023-06-06 07:34:25 +0500

    Correct query as per the remarks of the reviewer

--- queries.sql
+++ queries.sql
@@ -8,8 +8,11 @@ SELECT * FROM animals WHERE name LIKE '%mon';
 
 SELECT name
 FROM animals
-WHERE
-    date_of_birth BETWEEN '2016-01-01' AND '2019-01-01';
+WHERE EXTRACT(
+        year
+        FROM
+            date_of_birth
+    ) BETWEEN 2016 AND 2019;
 
 -- List the name of all animals that are neutered and have less than 3 escape attempts.
 
